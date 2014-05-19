551ec10a976dfc1148499803f1dab7971ebe8884
Hack Reactor Students <accounts@hackreactor.com>
2014-05-19 14:56:26 -0700

    adds JQueryUI Dates

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,36 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+
+  console.log('ready');
+
+  $( "#date1").datepicker({
+    onSelect: function(dateText, inst){
+      var dateAsString = dateText;
+      console.log(dateAsString);
+      var dateAsObject = $(this).datepicker("getDate");
+    $(".leaving").append(dateAsObject);
+    }
+  });
+
+  $( "#date2").datepicker({
+    onSelect: function(dateText, inst){
+      var dateAsString = dateText;
+      console.log(dateAsString);
+      var dateAsObject = $(this).datepicker("getDate");
+    $(".returning").append(dateAsObject);
+    }
+  });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
