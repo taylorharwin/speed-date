ee43f41f5e534034ba903d247b5aa695d3737c15
Hack Reactor Students <accounts@hackreactor.com>
2014-05-19 15:27:06 -0700

    full date selector implemented

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,8 +6,8 @@ $(function(){
     onSelect: function(dateText, inst){
       var dateAsString = dateText;
       console.log(dateAsString);
-      var dateAsObject = $(this).datepicker("getDate");
-    $(".leaving").append(dateAsObject);
+      var dateAsString = $(this).datepicker("getDate");
+      $(".leaving").append('<li class="times">' + moment(dateAsString).format("dddd, MMMM Do YYYY") + '</li>').fadeIn();
     }
   });
 
@@ -15,8 +15,8 @@ $(function(){
     onSelect: function(dateText, inst){
       var dateAsString = dateText;
       console.log(dateAsString);
-      var dateAsObject = $(this).datepicker("getDate");
-    $(".returning").append(dateAsObject);
+      var dateAsString = $(this).datepicker("getDate");
+    $(".returning").append('<li class="times">' + moment(dateAsString).format("dddd, MMMM Do YYYY") +'</li>').fadeIn();
     }
   });
 
