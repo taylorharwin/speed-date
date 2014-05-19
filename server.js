69d0b2e2e221bd20666a31a22012721f32d2c27a
Taylor Harwin <taylor.harwin@gmail.com>
2014-05-19 12:33:19 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser());
+app.set('public', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+
+var port = process.env.PORT;
+
+app.listen(port);
+
+console.log('Server now listening on port ' + port);
+
