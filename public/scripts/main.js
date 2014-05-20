8634df6710af81e984f17afce4ca10e85c9b7343
Hack Reactor Students <accounts@hackreactor.com>
2014-05-20 12:17:45 -0700

    aligns everything

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,12 @@
 $(function(){
 
 
-
   console.log('ready');
 
   $("#calbin1").hide();
   $("#calbin2").hide();
+  $('#chart').hide();
+
 
   $( "#date1").datepicker({
     onSelect: function(dateText, inst){
@@ -28,9 +29,22 @@ $(function(){
 $("#toggle").click(function() {
     $('[data-slidepanel]').slidepanel({
       orientation: 'top',
-      mode: 'overlay'
+      mode: 'blend'
     });
   });
+
+
+$('.showchart').on('click', function(){
+  console.log('clicks on button');
+  $('#chart').fadeIn(750);
+});
+
+
+$('.close').on('click', function(){
+    $('#chart').hide();
+});
+
+
 
 
 $("#may").on('click', function(){
@@ -45,9 +59,27 @@ $("#jun").on('click', function(){
 });
 
 
-// $("#feb").on('click', function(){
-//   $("#calbin2").datepicker(pickerOpts);
-// });
+var chart = c3.generate({
+  bindto: '#chart',
+      data: {
+        columns: [
+            ['Spring', 10],
+            ['Summer', 30],
+            ['Winter', 50],
+            ['Fall', 10],
+        ],
+        type : 'donut'
+    },
+    donut: {
+        title: "Popular Dates",
+        onclick: function (d, i) { console.log(d, i); },
+        onmouseover: function (d, i) { console.log(d, i); },
+        onmouseout: function (d, i) { console.log(d, i); }
+    }
+});
+
+
+
 
 
 });
